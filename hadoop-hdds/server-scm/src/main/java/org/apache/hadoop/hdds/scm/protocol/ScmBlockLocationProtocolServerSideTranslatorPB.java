d154073529f4b7e5a42e3bb18eb66ce278ca6493
Pavel Stepanov <ap0th3que@gmail.com>
2025-03-12 11:17:21 +0300

    Fix reading null datacenters from protobuf #2

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/protocol/ScmBlockLocationProtocolServerSideTranslatorPB.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/protocol/ScmBlockLocationProtocolServerSideTranslatorPB.java
@@ -202,7 +202,7 @@ public final class ScmBlockLocationProtocolServerSideTranslatorPB
                 request.getEcReplicationConfig()),
             request.getOwner(),
             ExcludeList.getFromProtoBuf(request.getExcludeList()),
-            request.getClient(), request.getDatacenters());
+            request.getClient(), request.hasDatacenters() ? request.getDatacenters() : null);
 
     AllocateScmBlockResponseProto.Builder builder =
         AllocateScmBlockResponseProto.newBuilder();
