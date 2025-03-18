7e787263b07688d2d5f8128ebe7d62abbca193f6
Pavel Stepanov <ap0th3que@gmail.com>
2025-03-18 13:58:12 +0300

    Disable BackgroundPipelineCreator

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/pipeline/BackgroundPipelineCreator.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/pipeline/BackgroundPipelineCreator.java
@@ -294,21 +294,23 @@ public class BackgroundPipelineCreator implements SCMService {
 
   @Override
   public boolean shouldRun() {
-    serviceLock.lock();
-    try {
-      // check one-short run
-      if (oneShotRun) {
-        oneShotRun = false;
-        return true;
-      }
+    return false;
+//    serviceLock.lock();
+//    try {
+//      // check one-short run
+//      if (oneShotRun) {
+//        oneShotRun = false;
+//        return true;
+//      }
 
       // If safe mode is off, then this SCMService starts to run with a delay.
-      return serviceStatus == ServiceStatus.RUNNING && (
-          createPipelineInSafeMode ||
-          clock.millis() - lastTimeToBeReadyInMillis >= waitTimeInMillis);
-    } finally {
-      serviceLock.unlock();
-    }
+//      return serviceStatus == ServiceStatus.RUNNING && (
+//          createPipelineInSafeMode ||
+//          clock.millis() - lastTimeToBeReadyInMillis >= waitTimeInMillis);
+//      return false;
+//    } finally {
+//      serviceLock.unlock();
+//    }
   }
 
   private boolean isOneShotRunNeeded() {
